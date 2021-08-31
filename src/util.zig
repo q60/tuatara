573591028e9d601ab6c3c1169ffbda2b4839c546
llathasa-veleth <llathasa@outlook.com>
2021-08-31 20:58:49 +0300

    Fix fileExists() leaks

--- src/util.zig
+++ src/util.zig
@@ -1,9 +1,10 @@
 usingnamespace @import("env.zig");
 
 pub fn fileExists(absolute_path: []const u8) bool {
-    _ = fs.openFileAbsolute(absolute_path, .{ .read = true }) catch {
+    const file = fs.openFileAbsolute(absolute_path, .{ .read = true }) catch {
         return false;
     };
+    file.close();
     return true;
 }
 
