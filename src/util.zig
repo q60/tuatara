08f22ce6a4fc4358aba778d04622e6ba8efddfc5
llathasa-veleth <llathasa@outlook.com>
2021-08-31 20:59:57 +0300

    Small code refinement

--- src/util.zig
+++ src/util.zig
@@ -1,9 +1,7 @@
 usingnamespace @import("env.zig");
 
 pub fn fileExists(absolute_path: []const u8) bool {
-    const file = fs.openFileAbsolute(absolute_path, .{ .read = true }) catch {
-        return false;
-    };
+    const file = fs.openFileAbsolute(absolute_path, .{ .read = true }) catch return false;
     file.close();
     return true;
 }
