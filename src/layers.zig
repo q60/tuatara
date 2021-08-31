388ba2d00b365058e7fd753f568dbb61906aa12e
llathasa-veleth <llathasa@outlook.com>
2021-08-31 16:14:20 +0300

    Use more convenient name for std.mem

--- src/layers.zig
+++ src/layers.zig
@@ -1,6 +1,6 @@
 usingnamespace @import("env.zig");
 
-pub fn osName(allocator: *std.mem.Allocator) ![]const u8 {
+pub fn osName(allocator: *mem.Allocator) ![]const u8 {
     const file = fs.openFileAbsolute("/etc/os-release", .{ .read = true }) catch {
         return "unknown";
     };
@@ -8,14 +8,14 @@ pub fn osName(allocator: *std.mem.Allocator) ![]const u8 {
     const file_read = try file.readToEndAlloc(allocator, 0x200);
     defer allocator.free(file_read);
 
-    var lines = std.mem.tokenize(u8, file_read, "\n");
+    var lines = mem.tokenize(u8, file_read, "\n");
     var os_name: []const u8 = undefined;
     const os_name_prefix =
         \\PRETTY_NAME="
     ;
     while (true) {
         var line = lines.next() orelse break;
-        if (std.mem.startsWith(u8, line, os_name_prefix)) {
+        if (mem.startsWith(u8, line, os_name_prefix)) {
             os_name = line[os_name_prefix.len .. line.len - 1];
             break;
         }
@@ -24,6 +24,6 @@ pub fn osName(allocator: *std.mem.Allocator) ![]const u8 {
     const res = try allocator.alloc(u8, os_name.len);
     errdefer allocator.free(res);
 
-    std.mem.copy(u8, res, os_name);
+    mem.copy(u8, res, os_name);
     return res;
 }
--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,7 @@ pub fn main() anyerror!void {
     var info = List([][]const u8).init(alloc);
     defer info.deinit();
 
-    var home = string.tokenize(u8, os.getenv("HOME").?, fs.path.sep_str);
+    var home = mem.tokenize(u8, os.getenv("HOME").?, fs.path.sep_str);
     var username: []const u8 = undefined;
     while (true) {
         username = home.next() orelse break;
@@ -23,7 +23,7 @@ pub fn main() anyerror!void {
     var buf: [os.HOST_NAME_MAX]u8 = undefined;
     const hostname = try os.gethostname(&buf);
 
-    const user_at_host = try string.concat(alloc, u8, &[_][]const u8{ username, " @ ", hostname });
+    const user_at_host = try mem.concat(alloc, u8, &[_][]const u8{ username, " @ ", hostname });
     defer alloc.free(user_at_host);
     try info.append(&[_][]const u8{ user_at_host, "[user]" });
 
