3251a7e407b0bb9f0901f9a69cc7886e5a2ced24
llathasa-veleth <llathasa@outlook.com>
2021-08-31 20:40:51 +0300

    Add rightAlign() to util.zig

--- src/util.zig
+++ src/util.zig
@@ -6,3 +6,15 @@ pub fn fileExists(absolute_path: []const u8) bool {
     };
     return true;
 }
+
+pub fn rightAlign(allocator: *mem.Allocator, maxlen: usize) ![]const u8 {
+    var indent = List(u8).init(allocator);
+    defer indent.deinit();
+
+    var i: usize = 0;
+    while (i <= maxlen) : (i += 1) {
+        try indent.append(0x20);
+    }
+
+    return indent.toOwnedSlice();
+}
